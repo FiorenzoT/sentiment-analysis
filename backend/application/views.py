d8f291ace6b969f9cf63c04ab17538ca8eee33ae
Fiorenzo Tittaferrante <102415167+FiorenzoT@users.noreply.github.com>
2024-10-07 20:08:39 +0200

    Implement sentiment_analysis view.

--- backend/application/views.py
+++ backend/application/views.py
@@ -1,9 +1,30 @@
+import json
+
+from django.http import JsonResponse
 from django.shortcuts import render
 from textblob import TextBlob
 
 
 def sentiment_analysis(request):
 
-    # TODO
+    if request.method == 'POST':
+        input = request.POST.get('text')
+
+        # Analyze the input
+        blob = TextBlob(input)
+        sentiment_polarity = blob.sentiment.polarity
+
+        if sentiment_polarity > 0:
+            sentiment = 'Positive'
+        elif sentiment_polarity < 0:
+            sentiment = 'Negative'
+        else:
+            sentiment = 'Neutral'
+    
+        return render(request, 'application/result.html', {
+            'sentiment': sentiment,
+            'popolarity': sentiment_polarity,
+            'text': input
+        })
     
     return render(request, 'application/index.html')

e3c972018df4c60102680b370f2ef260c0397bf8
Fiorenzo Tittaferrante <102415167+FiorenzoT@users.noreply.github.com>
2024-10-08 18:34:08 +0200

    Change url path.

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('application/', include('application.urls'))
+    path('', include('application.urls'))
 ]
